9e06ea597fd6607f26320fe54b0c039aa20c76d0
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-04 06:09:49 +0530

    Button Signup UI

--- authenticationsystem/src/app/signup/page.tsx
+++ authenticationsystem/src/app/signup/page.tsx
@@ -16,8 +16,8 @@ export default function SignupPage() {
 
     }
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2"> 
-        <h1>Signup</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 font-bold"> 
+        <h1>SIGNUP</h1>
         <hr />
         <label htmlFor="username" >User Name</label>
         <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
@@ -43,6 +43,10 @@ export default function SignupPage() {
             onChange={(e) => setUser({...user,password:e.target.value})}
             placeholder="password"
         />
+        <button onClick={onSignup} 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outine-none focus:border-gray-600
+        " >Signup Here</button>
+        <Link href="/login">Visit Login Page</Link>
         </div>
         
     )
