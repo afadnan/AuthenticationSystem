edd3a33c2bd5fbe25cc945a0e1da6667f3435fd7
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-07 02:07:39 +0530

    user exits and compare password

--- authenticationsystem/src/app/api/users/login/route.ts
+++ authenticationsystem/src/app/api/users/login/route.ts
@@ -7,5 +7,27 @@ import bcryptjs from "bcryptjs";
 connect();
 
 export async function POST(request:NextRequest){
-    
+    try {
+        
+        const reqBody =await request.json();
+        const {email,password} = reqBody;
+        console.log(reqBody);
+
+        //check if user exists
+        const user = await User.findOne({email});
+        if(!user){
+            return NextResponse.json({error:"User does not exit"},{status:400});
+        }
+
+        //check if password is correct
+        const validPassword = await bcryptjs.compare(password,user.password);
+        if(!validPassword){
+            return NextResponse.json({error:"Invalid password"}, {status:400})
+        }
+        
+        
+    } catch (error:any) {
+        return NextResponse.json({error: error.message},{status:500})
+        
+    }
 }
