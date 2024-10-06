52bfcd989b7df4be4ce6a7208834f4c4b5edca23
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-07 03:34:48 +0530

    login page/route fixed -> user route to profile

--- authenticationsystem/src/app/api/users/login/route.ts
+++ authenticationsystem/src/app/api/users/login/route.ts
@@ -3,6 +3,7 @@ import User from "@/models/userModel";
 import { NextRequest,NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+
 connect();
 
 export async function POST(request:NextRequest){
@@ -17,9 +18,12 @@ export async function POST(request:NextRequest){
         if(!user){
             return NextResponse.json({error:"User does not exit"},{status:400});
         }
+        console.log("user exists");
+        
 
         //check if password is correct
-        const validPassword = await bcryptjs.compare(password,user.password);
+        const validPassword = await bcryptjs.compare(password, user.password);
+        console.log("Password valid:", validPassword);
         if(!validPassword){
             return NextResponse.json({error:"Invalid password"}, {status:400})
         }
@@ -27,13 +31,22 @@ export async function POST(request:NextRequest){
         //create token data
         const tokenData = {
             id: user._id,
-            username:user.useanme,
+            username:user.username,
             email:user.email
         }
         //create token
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!,{expiresIn:"1d"})
+        const response = NextResponse.json({
+            message:"Login succesful",
+            success:true,
+        })
 
-        
+        response.cookies.set("token",token, {
+            httpOnly:true,
+        })
+        return response;
+
+
     } catch (error:any) {
         return NextResponse.json({error: error.message},{status:500})
         
--- authenticationsystem/src/app/login/page.tsx
+++ authenticationsystem/src/app/login/page.tsx
@@ -1,25 +1,57 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios";
-
+import { NextResponse } from "next/server";
+import toast from "react-hot-toast";
 
 
 export default function LoginPage() {
+    const router = useRouter(); //csz pushing to different page
     const [user,setUser] = useState({
         email:"",
         password:"",
     })
 
-    const onLogin = async () => {}
+    const [buttonDisabled,setButtonDisabled] = React.useState(false);
+
+    const [loading,setLoading] = useState(false);
+
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login",user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
+            router.push("/profile");
+            
+        } catch (error :any) {
+            console.log("Login failed",error.message);
+            toast.error(error.message);
+            
+        }finally{
+            setLoading(false);
+        }
+
+    }
+
+    useEffect(()=> {
+        if(user.email.length > 0 && user.password.length > 0 ){
+            setButtonDisabled(false);
+        }else {
+            setButtonDisabled(true);
+        }
+    },[user])
+
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 font-black"> 
-        <h1 className="m-5">LOGIN</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 font-black "> 
+        <h1 className="m-5"> {loading ? "Processing..." : "LOGIN"} </h1>
         <hr />
         <label htmlFor="email" >Email</label>
-        <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="email"
             type ="text"
             value={user.email}
@@ -27,7 +59,7 @@ export default function LoginPage() {
             placeholder="email"
         />
         <label htmlFor="password" >Password</label>
-        <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="password"
             type ="password"
             value={user.password}
@@ -36,7 +68,7 @@ export default function LoginPage() {
         />
         <button onClick={onLogin} 
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outine-none focus:border-gray-600
-        " >Login Here</button>
+        " > {buttonDisabled ? "No Login" : "Login Here"} </button>
         <Link href="/signup">Visit Signup Page</Link>
         </div>
         
