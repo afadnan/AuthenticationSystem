11ff3d76847152ae9bd2ea1962a7d5e35b52ff2d
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-07 01:49:07 +0530

    Saved user signup data on DB

--- authenticationsystem/src/app/signup/page.tsx
+++ authenticationsystem/src/app/signup/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
     const router = useRouter();
@@ -14,7 +15,23 @@ export default function SignupPage() {
     })
     const [buttonDisabled,setButtonDisabled] = React.useState(false);
 
+    const [loading,setLoading] = useState(false);
+
     const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup",user);
+            console.log("Signup success",response.data);
+
+            router.push("/login");
+            
+        } catch (error :any) {
+            console.log("signup failed",error.message);
+            
+            toast.error(error.message);
+        }finally{
+            setLoading(false);
+        }
 
     }
     useEffect(()=> {
@@ -26,7 +43,7 @@ export default function SignupPage() {
     },[user])
     return (
         <div className="flex  flex-col items-center justify-center min-h-screen py-2 font-black"> 
-        <h1 className="m-5">SIGNUP</h1>
+        <h1 className="m-5"> {loading ? "Loading..." : "SIGNUP"} </h1>
         <hr />
         <label htmlFor="username" >User Name</label>
         <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
--- authenticationsystem/src/dbConfig/dbConfig.ts
+++ authenticationsystem/src/dbConfig/dbConfig.ts
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export async function connect() {
     try {
+        console.log('MONGO_URI:', process.env.MONGO_URI);
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
 
