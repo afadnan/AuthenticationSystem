a141a62e4268f5c75d48a79913a7cb68cbf06979
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-06 19:10:59 +0530

    if user already exists

--- authenticationsystem/src/app/api/users/signup/route.ts
+++ authenticationsystem/src/app/api/users/signup/route.ts
@@ -5,3 +5,22 @@ import bcryptjs from "bcryptjs";
 
 
 connect()
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody = await request.json();
+        const {username, email, password} =reqBody;
+
+        console.log(reqBody);
+
+        //check if user alredy exists
+        const user = await User.findOne({email});
+        if(user) {
+            return NextResponse.json({error: "User already exists"},{status:400})
+        }
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},{status:500})
+        
+    }
+}
