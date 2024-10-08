d3321c16ffa61347d9eb71e56a7dd98a58364803
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-08 08:01:59 +0530

    logout page i.e logout button on frontend

--- authenticationsystem/src/app/profile/page.tsx
+++ authenticationsystem/src/app/profile/page.tsx
@@ -1,16 +1,36 @@
-
+"use client"
 
 import React from "react"
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";;
+import { useRouter } from "next/navigation";
 
 export default function Userprofile(){
+    const router = useRouter();
+    const logout = async () => {
+
+        
+        try {
+            await axios.get('/api/users/logout');
+            toast.success('Logout successful');
+            router.push('/login');
+        } catch (error : any) {
+            console.log(error.messaage);
+            toast.error(error.message);
+            
+        }
+
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>
                 Profile
             </h1>
             <hr />
-            <p className="text-4xl">Profile Page 
-                </p>
+            <p className="text-4xl">Profile Page </p>
+            <hr />
+            <button onClick={logout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Logout</button>
         </div>
     )
 }
