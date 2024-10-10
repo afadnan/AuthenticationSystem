5b93552b79ea987db32e4641da38d0f05d70be22
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-10 11:36:16 +0530

    retrieves user data based on a token

---
+++ authenticationsystem/src/app/api/users/me/route.ts
@@ -0,0 +1,6 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest,NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from "@/dbConfig/dbConfig";
+
+export default
