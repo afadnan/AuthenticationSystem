fe609cd590b8db4bf800711a4c0f563dc3dbbee9
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-15 07:28:57 +0530

    hasedToken,Verify,Reset Token

--- authenticationsystem/src/helpers/mailer.ts
+++ authenticationsystem/src/helpers/mailer.ts
@@ -1,3 +1,21 @@
 import nodemailer from 'nodemailer';
 import User from '@/models/userModel';
 import bcryptjs from 'bcryptjs';
+
+export const sendEmail = async({email,emailType,userId}: any) =>{
+    try {
+        // create a hased token
+        const hasedToken = await bcryptjs.hash(userId.toString(),10);
+
+        if(emailType === "VERIFY"){
+            await User.findByIdAndUpdate(userId,{verifyToken: hasedToken,verifyTokenExpiry: Date.now() + 3600000
+        })
+        
+    }else if (emailType === "RESET"){
+            await User.findByIdAndUpdate(userId,{forgetPasswordToken : hasedToken,forgetPasswordTokenExpiry:Date.now()+ 3600000})
+        }
+
+    } catch (error : any) {
+        throw new Error(error.message);
+    }
+}
