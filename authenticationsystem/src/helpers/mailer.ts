437a8add9219e0523458ed289454a892c93dc036
Adnan Ashraf <af.adnan23@gmail.com>
2024-10-17 15:39:11 +0530

    able to get verification link on mailtrap

--- authenticationsystem/src/helpers/mailer.ts
+++ authenticationsystem/src/helpers/mailer.ts
@@ -31,7 +31,9 @@ export const sendEmail = async({email,emailType,userId}: any) =>{
     from : 'er.adnan16@gmail.com',
     to: email,
     subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-    html: `<p>Click <a href="${process.env.domain}/verifyemail?token=${hashedToken}">here</a> to {emailType === "VERIFY" ? "verify your email" : "reset your password"} </p>`
+    html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"} 
+    or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+    </p>`
   }
 
   const mailresponse = await transport.sendMail(mailOptions);
